c2586b5a5e1fc5f6a158d4184346836dce82e925
Ziyao Zhang <ziyaointl@gmail.com>
2018-01-23 08:43:46 +0000

    Implement Event Model

--- django/SEHSCalendar/main/models.py
+++ django/SEHSCalendar/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Event model
+class Event(models.Model):
+    title = models.CharField(max_length = 255)
+    description = models.TextField()
+    last_modified = models.DateTimeField(auto_now = True)
+    event_date = models.DateTimeField()
