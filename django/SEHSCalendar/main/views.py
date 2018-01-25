8ae5b3173a8ae677969ea1bd7cc82ae364fd87ac
Ziyao Zhang <ziyaointl@gmail.com>
2018-01-25 02:04:04 +0000

    Add index view

--- django/SEHSCalendar/main/views.py
+++ django/SEHSCalendar/main/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
+from .models import Event
 
+def index(request):
+    all_events = Event.objects.all()
+    next_two_events = Event.objects.order_by('-event_date')[:2]
+    return render(request, 'main/index.html', {
+        'next_two_events': next_two_events
+    })
