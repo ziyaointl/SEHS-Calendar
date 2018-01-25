4390edd5128610b06afe64103d5ec10680373ba9
Ziyao Zhang <ziyaointl@gmail.com>
2018-01-25 06:54:49 +0000

    Update model to reflect event duration

--- django/SEHSCalendar/main/models.py
+++ django/SEHSCalendar/main/models.py
@@ -1,8 +1,29 @@
 from django.db import models
+from datetime import timedelta
 
 # Event model
 class Event(models.Model):
     title = models.CharField(max_length = 255)
     description = models.TextField()
+    all_day = models.BooleanField('All Day Event', default = False)
     last_modified = models.DateTimeField(auto_now = True)
-    event_date = models.DateTimeField()
+    date = models.DateTimeField()
+    duration = models.DurationField(default = timedelta(hours = 1))
+    
+    def get_title(self):
+        return self.title
+        
+    def get_month(self):
+        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        return months[self.date.month - 1]
+
+    def get_start_time(self):
+        start_hour = str(self.date.hour)
+        start_minute = '{:02d}'.format(self.date.minute)
+        return start_hour + ':' + start_minute
+        
+    def get_finish_time(self):
+        finish_date = self.date + self.duration
+        finish_hour = str(finish_date.hour)
+        finish_minute = '{:02d}'.format(finish_date.minute)
+        return finish_hour + ':' + finish_minute
--- django/SEHSCalendar/main/views.py
+++ django/SEHSCalendar/main/views.py
@@ -3,7 +3,7 @@ from .models import Event
 
 def index(request):
     all_events = Event.objects.all()
-    next_two_events = Event.objects.order_by('-event_date')[:2]
+    next_two_events = Event.objects.order_by('date')[:2]
     return render(request, 'main/index.html', {
         'next_two_events': next_two_events
     })
